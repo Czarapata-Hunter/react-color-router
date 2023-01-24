fe6b8671d84616c934447f5c2731de86e3c3b29b
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-24 10:43:15 -0800

    Quick layout of the main styling of the pages with header, footer, and main added into App

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>Hunter Czarapata 2023</p>
+    </div>
+  );
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div>
+      <div className="header">
+        <p>Color 1</p>
+        <p>Color 2</p>
+        <p>Color 3</p>
+      </div>
+    </div>
+  );
+}
